31bfce635f97e15751b7a96e7be5e2771fc047b7
Laurent Bernaille <laurent.bernaille@gmail.com>
2016-08-09 18:02:04 +0200

    dump consul keys for docker overlay

---
+++ dump_endpoints.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import consul
+import json
+
+c=consul.Consul(host="localhost",port=8500)
+
+(idx,endpoints)=c.kv.get("docker/network/v1.0/endpoint/",recurse=True)
+epdata=[ ep['Value'] for ep in endpoints if ep['Value'] is not None]
+
+for data in epdata:
+  print(json.dumps(json.loads(data.decode("utf-8")), indent=4, sort_keys=True))

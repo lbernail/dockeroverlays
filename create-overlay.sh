f2575d2cad7bdcf0bac03f073e5ca415a4565be0
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-04-11 19:06:54 +0200

    Factorize code

--- create-overlay.sh
+++ create-overlay.sh
@@ -1,9 +1,11 @@
-sudo ip netns delete overns 2> /dev/null && "Deleting existing overlay"
+options=${1:-}
+
+sudo ip netns delete overns 2> /dev/null && echo "Deleting existing overlay"
 sudo ip netns add overns
 sudo ip netns exec overns ip link add dev br0 type bridge
 sudo ip netns exec overns ip addr add dev br0 192.168.0.1/24
 
-sudo ip link add dev vxlan1 type vxlan id 42 proxy learning dstport 4789
+sudo ip link add dev vxlan1 type vxlan id 42 proxy learning $options dstport 4789
 sudo ip link set vxlan1 netns overns
 sudo ip netns exec overns ip link set vxlan1 master br0
 

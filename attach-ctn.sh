b7ac2986018c8730568d581ac99ccfc44f2ef82d
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-04-11 18:57:06 +0200

    Reorganize repo

---
+++ attach-ctn.sh
@@ -0,0 +1,9 @@
+ctn=${1:-demo}
+ip=${2:-2}
+
+ctn_ns_path=$(docker inspect --format="{{ .NetworkSettings.SandboxKey}}" $ctn)
+ctn_ns=${ctn_ns_path##*/}
+sudo ip link set dev veth2 netns $ctn_ns
+sudo ip netns exec $ctn_ns ip link set dev veth2 name eth0 address 02:42:c0:a8:00:0${ip}
+sudo ip netns exec $ctn_ns ip addr add dev eth0 192.168.0.${ip}/24
+sudo ip netns exec $ctn_ns ip link set dev eth0 up

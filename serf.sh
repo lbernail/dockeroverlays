9e3d7ebcbef021cf2e4759340d5d67ac58219086
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-04-25 17:14:58 +0200

    improve display

--- serf.sh
+++ serf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+echo
 echo "#########################################"
 echo "#########################################"
-
 echo "New event: ${SERF_EVENT}"
 while read line; do
-    printf "${line}\n\n"
+    printf "${line}\n"
 done
-
 echo "#########################################"
 echo "#########################################"
+echo

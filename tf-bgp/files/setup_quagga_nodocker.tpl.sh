03548eba0570a794166e278ce52b2fa0e31b260a
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-10-15 11:06:44 +0200

    Configuration without Docker (with working Cumulus version)

--- tf-bgp/files/setup_quagga_nodocker.tpl.sh
+++ tf-bgp/files/setup_quagga_nodocker.tpl.sh
@@ -6,17 +6,11 @@ echo ${TF_HOSTNAME} > /etc/hostname
 echo "${TF_HOST_IP} ${TF_HOSTNAME}" >> /etc/hosts
 hostname ${TF_HOSTNAME}
 
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y apt-transport-https ca-certificates tcpdump ethtool vim curl python-pip jq unzip iputils-arping
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install -y docker-engine
-
-echo "Adding ${TF_USER} user to docker group"
-gpasswd -a ${TF_USER} docker
+apt-get install -y tcpdump ethtool vim curl iputils-arping bridge-utils iptables iproute libjson-c2 logrotate python python-ipaddr
 
 echo "Adding a few helpers for demo purposes"
 curl -sSL https://github.com/lbernail/dockercon2017/archive/master.tar.gz | tar -xzf - --strip-components=1 -C /home/${TF_USER}
@@ -24,9 +18,18 @@ chown -R ${TF_USER}:${TF_USER} /home/${TF_USER}
 
 echo "Installing cumulus Quagga"
 apt-get install -y iproute libsnmp30
-curl -sSL http://cumulusfiles.s3.amazonaws.com/roh-3.3.2-ubuntu1604.tar | tar -xC /tmp
-dpkg -i /tmp/quagga_*
+#curl -sSL http://cumulusfiles.s3.amazonaws.com/roh-3.3.2-ubuntu1604.tar | tar -xC /tmp
+#dpkg -i /tmp/ubuntu1604/quagga_*
+curl -sSLo /tmp/quagga.deb http://repo3.cumulusnetworks.com/repo/pool/cumulus/q/quagga/quagga_1.0.0+cl3eau8_amd64.deb
+curl -sSLo /tmp/quagga-dbg.deb http://repo3.cumulusnetworks.com/repo/pool/cumulus/q/quagga/quagga-dbg_1.0.0+cl3eau8_amd64.deb
+curl -sSLo /tmp/quagga-doc.deb http://repo3.cumulusnetworks.com/repo/pool/cumulus/q/quagga/quagga-doc_1.0.0+cl3eau8_all.deb
 
+dpkg -i /tmp/quagga*.deb
+
+echo "Generating Quagga configuration"
+cat <<EOF > /etc/quagga/Quagga.conf
+${TF_QUAGGA_CONF}
+EOF
 cat <<EOF > /etc/quagga/daemons
 zebra=yes
 bgpd=yes
@@ -37,6 +40,11 @@ ripngd=no
 isisd=no
 pimd=no
 ldpd=no
+EOF
+cat <<EOF > /etc/quagga/debian.conf
+vtysh_enable=yes 
+zebra_options=" --daemon -A 127.0.0.1 "
+bgpd_options=" --daemon -A 127.0.0.1 "
 EOF
 
 systemctl enable quagga

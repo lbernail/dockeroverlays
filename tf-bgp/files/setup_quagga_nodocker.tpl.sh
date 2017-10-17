4c828b453780499c90619f0e6c45c3391d752ca5
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-10-17 16:46:15 +0200

    Add default gateway for example containers. Add Linuxkit image

--- tf-bgp/files/setup_quagga_nodocker.tpl.sh
+++ tf-bgp/files/setup_quagga_nodocker.tpl.sh
@@ -13,8 +13,16 @@ echo "Installing dependencies..."
 apt-get update
 apt-get install -y tcpdump ethtool vim curl iputils-arping bridge-utils iptables iproute libjson-c2 logrotate python python-ipaddr qemu
 
-echo "Downloading ubuntu img"
+echo "Downloading ubuntu and linuxkit img"
 curl -sSLo /home/${TF_USER}/wheezy.qcow2 https://people.debian.org/~aurel32/qemu/i386/debian_wheezy_i386_standard.qcow2
+
+mkdir -p /home/${TF_USER}/linuxkit/bin
+for f in bin/linuxkit linuxkit-cmdline  linuxkit-initrd.img  linuxkit-kernel
+do
+    curl -sSLo /home/${TF_USER}/linuxkit/$f http://dockercon.awsdemo.d2-si.eu.s3-website-eu-west-1.amazonaws.com/$f
+done
+chmod +x /home/${TF_USER}/linuxkit/bin/*
+cp /home/${TF_USER}/linuxkit/bin/* /usr/local/bin
 
 echo "Adding a few helpers for demo purposes"
 curl -sSL https://github.com/lbernail/dockercon2017/archive/master.tar.gz | tar -xzf - --strip-components=1 -C /home/${TF_USER}
@@ -22,8 +30,6 @@ chown -R ${TF_USER}:${TF_USER} /home/${TF_USER}
 
 echo "Installing cumulus Quagga"
 apt-get install -y iproute libsnmp30
-#curl -sSL http://cumulusfiles.s3.amazonaws.com/roh-3.3.2-ubuntu1604.tar | tar -xC /tmp
-#dpkg -i /tmp/ubuntu1604/quagga_*
 curl -sSLo /tmp/quagga.deb http://repo3.cumulusnetworks.com/repo/pool/cumulus/q/quagga/quagga_1.0.0+cl3eau8_amd64.deb
 curl -sSLo /tmp/quagga-dbg.deb http://repo3.cumulusnetworks.com/repo/pool/cumulus/q/quagga/quagga-dbg_1.0.0+cl3eau8_amd64.deb
 curl -sSLo /tmp/quagga-doc.deb http://repo3.cumulusnetworks.com/repo/pool/cumulus/q/quagga/quagga-doc_1.0.0+cl3eau8_all.deb

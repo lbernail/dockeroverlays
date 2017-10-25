d75a870a3877a7df43ec548b7a2ba7d2763bb8f8
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-10-25 15:03:41 +0200

    Fix security group

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -10,6 +10,14 @@ resource "aws_security_group" "consul" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8500
+    to_port     = 8500
+    protocol    = "tcp"
+    security_groups = ["${aws_security_group.docker.id}"]
+  }
+
+
   egress {
     from_port       = 0
     to_port         = 0
